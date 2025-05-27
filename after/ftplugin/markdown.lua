8fa8fed076bae2ed3e05c2f22b3f0e228b4d99e0
Iuro Nascimento <iurobpn@proton.me>
2025-05-27 18:09:38 -0300

    add syntax highlight file

---
+++ after/ftplugin/markdown.lua
@@ -0,0 +1,9 @@
+local color = require'gruvbox.colors'.get_colors()
+local palette = vim.g.gruvbox_palette
+-- Define the highlight group
+vim.api.nvim_set_hl(0, 'TaskUUID', { fg = palette.bright_orange, bg = palette.dark1, bold = true })
+
+-- Link the group to the pattern
+vim.cmd('syntax match TaskUUID /@{[0-9a-fA-F-]\\+}/')
+print('TaskUUID syntax highlighting enabled')
+
