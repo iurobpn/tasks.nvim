93732918383d50e743497a43548bfa356fb9a7b0
Iuro Nascimento <iurobpn@proton.me>
2025-05-28 10:58:26 -0300

    add markdown syntax

--- lua/tasks/syntax/markdown.lua
+++ lua/tasks/syntax/markdown.lua
@@ -3,6 +3,6 @@ local palette = vim.g.gruvbox_palette
 vim.api.nvim_set_hl(0, 'TaskUUID', { fg = palette.bright_orange, bg = palette.dark1, bold = true })
 
 -- Link the group to the pattern
-vim.cmd('syntax match TaskUUID /@{.*}/')
+vim.cmd('syntax region TaskUUID matchgroup=xParen start=/@{/ end=/}/')
 print('TaskUUID syntax highlighting enabled')
 
