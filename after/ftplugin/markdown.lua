e9504a64b62c5a578dc48f08570c75556a6cfead
Iuro Nascimento <iurobpn@proton.me>
2025-06-03 17:59:34 -0300

    moving syntax files to autocmd

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,8 +1,8 @@
-local palette = vim.g.gruvbox_palette
--- Define the highlight group
-vim.api.nvim_set_hl(0, 'TaskUUID', { fg = palette.bright_orange, bg = palette.dark1, bold = true })
-
--- Link the group to the pattern
-vim.cmd('syntax match TaskUUID /@{.*}/')
-print('TaskUUID syntax highlighting enabled')
-
+-- local palette = vim.g.gruvbox_palette
+-- -- Define the highlight group
+-- vim.api.nvim_set_hl(0, 'TaskUUID', { fg = palette.bright_orange, bg = palette.dark1, bold = true })
+--
+-- -- Link the group to the pattern
+-- vim.cmd('syntax match TaskUUID /@{.*}/')
+-- print('TaskUUID syntax highlighting enabled')
+--
---
+++ syntax.lua
@@ -0,0 +1,18 @@
+vim.cmd([[
+" Add to your vimrc (or create a plugin file)
+augroup custom_markdown_syntax
+  autocmd!
+  autocmd Syntax markdown ~/.vim/after/syntax/markdown.vim
+augroup END
+]])
+
+
+-- autogroup command in lua
+vim.api.nvim_create_augroup("custom_markdown_syntax", { clear = true })
+vim.api.nvim_create_autocmd("Syntax", {
+  pattern = "markdown",
+  callback = function()
+    require"tasks.markdown")
+  end,
+  group = "custom_markdown_syntax",
+})
